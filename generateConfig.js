722fe51a54eafa0e74a58c18ded789a4a4e237c8
Abhinash04 <abhinash.pritiraj@gmail.com>
2025-06-19 01:18:44 +0530

    api hidden

--- generateConfig.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
-dotenv.config();
-
-const configContent = `const GEMINI_API_KEY = "${process.env.GEMINI_API_KEY}";`;
-
-fs.writeFileSync('/config.js', configContent);
-console.log('config.js generated successfully.');
